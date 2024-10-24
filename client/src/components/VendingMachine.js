00ab55447c3af9e542776c10d7c4748a758d0add
Dia Seung <diana.seung@gmail.com>
2024-10-23 22:28:43 -0700

    create the necessary files for project

---
+++ client/src/components/VendingMachine.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const VendingMachine = () => {
+    return(
+        // Beverage Vending Machine Criteria
+        // Three different beverages, qty 5 of each. Price is 2 quarters ($0.50) per drink. 
+        // only quarters accepted, only one at a time
+        // only 1 beverage per transaction
+        // Unused quarters returned to user
+        <div>
+            <h1>Vend-O-Matic</h1>
+        </div>
+    )
+};
+
+export default VendingMachine;
